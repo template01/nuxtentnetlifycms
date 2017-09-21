ceacfa98a198acbcdcbd95f1df5c2ed6966707f6
lasers laptop <contact@template01.info>
2017-09-21 23:51:22 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  head: {
+    title: 'Nuxtent Starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxtent project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  build: {
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+
+  modules: [
+    "nuxt-netlify-cms",
+    ['nuxtent']
+  ],
+  loading: { color: '#3B8070' }
+}
